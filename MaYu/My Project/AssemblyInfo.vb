2c65bb9f85949859260393f396c62272a2ed8337
malikalamsyah99 <60433700+malikalamsyah99@users.noreply.github.com>
2020-02-11 22:04:35 +0700

    Add files via upload

---
+++ MaYu/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Alarm Sekolah")> 
+<Assembly: AssemblyDescription("Aplikasi Alarm bel sekolah untuk SD, SMP dan SMA.  Created and designed by Malik Alamsyah (15083000108) & Bayu Muhammad Yamin (1508300060)")> 
+<Assembly: AssemblyCompany("MaYu Blessing Software")> 
+<Assembly: AssemblyProduct("Alarm Sekolah")> 
+<Assembly: AssemblyCopyright("Copyright ©  MaYu Blessing Software 2017")> 
+<Assembly: AssemblyTrademark("MaYu Blessings Software")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c7b67616-56db-4764-8501-ece233fa00f4")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("5.5.2.1")> 
